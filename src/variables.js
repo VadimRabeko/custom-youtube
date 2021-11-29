aab615de34d04dc058e507b1d5e2c6967848f705
VadimRabeko <vadimrabeko@gmail.com>
2021-11-29 13:13:19 +0300

    update API key

--- src/variables.js
+++ src/variables.js
@@ -1,4 +1,4 @@
-export const API_KEY = 'AIzaSyArve-iFrcSKZhr2z2SMOBSPoXQrwJbW0w';
+export const API_KEY = 'AIzaSyB0dMZWtwdfEmiCyVQsEGyjDtPuzWlH9co';
 export const MAX_RESULTS = 10;
 export const INPUT = document.querySelector('input');
 export const SUBMIT_BUTTON = document.querySelector('button');
